6241c4e0315e46bbf6002e9f9f380f29a713f038
bajor-szabolcs <szabialt12321@gmail.com>
2025-10-22 09:09:49 +0200

    Updated index file

--- api/services/index.js
+++ api/services/index.js
@@ -1,10 +1,12 @@
 const UserService = require("./UserService")
 const GameService = require("./GameService")
+const WorkshopService = require("./WorkshopService")
 
 module.exports = (db) =>
 {
     const userService = new UserService(db);
     const gameService = new GameService(db);
+    const workshopService = new WorkshopService(db)
 
-    return { userService, gameService };
+    return { userService, gameService, workshopService };
 }
